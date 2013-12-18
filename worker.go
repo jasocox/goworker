ff1bf6029aada8c1c7eb3b639801de33f12e5226
Jason Cox <JasonWilburCox@gmail.com>
2013-12-18 00:44:16 -0500

    Moving all the generic worker code for the recipes project this project for reusage

---
+++ worker.go
@@ -0,0 +1,63 @@
+package goworker
+
+import "errors"
+
+type Worker interface {
+  Name() string
+  Messages() chan string
+  Running() bool
+
+  MustStart()
+  Start() error
+}
+
+type worker struct {
+  name string
+  messages chan string
+  running bool
+  task func()
+}
+
+func NewWorker(name string, task func()) Worker {
+  w := new(worker)
+  w.name = name
+  w.task = task
+
+  return w
+}
+
+func (w *worker) Name() string {
+  return w.name
+}
+
+func (w *worker) Messages() chan string {
+  return w.messages
+}
+
+func (w *worker) Running() bool {
+  return w.running
+}
+
+func (w *worker) MustStart() {
+  err := w.Start()
+  if err != nil {
+    panic(err.Error())
+  }
+}
+
+func (w *worker) Start() (err error) {
+  if w.running {
+    err = errors.New(w.name + " is already running")
+    return
+  }
+
+  w.running = true
+  go func() {
+    w.task()
+
+    w.running = false
+    w.messages <- "Done"
+  }()
+
+  return
+}
