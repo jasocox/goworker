928bb82664c24d4078c11d5134e56e29477945e5
Jason Cox <JasonWilburCox@gmail.com>
2014-01-01 15:20:09 -0500

    More tests

--- task_test.go
+++ task_test.go
@@ -5,3 +5,20 @@ import "testing"
 func TestTaskCreation(t *testing.T) {
   NewTask(func() error {return nil})
 }
+
+func TestTaskRunning(t *testing.T) {
+  var val *bool
+
+  val = new(bool)
+
+  task := NewTask(func() error {
+    *val = true
+    return nil
+  })
+
+  task.Do()
+
+  if !*val {
+    t.Error("Didn't exec the task")
+  }
+}
