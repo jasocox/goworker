e9858b9abcc568c10178303ca1ea9caddbec4298
Jason Cox <JasonWilburCox@gmail.com>
2014-01-15 09:58:30 -0500

    More testing

--- manager_test.go
+++ manager_test.go
@@ -10,3 +10,18 @@ func Test_New(t *testing.T) {
     return
   }
 }
+
+func Test_ManagerDoesTask( t *testing.T) {
+  m := NewManager("Test Manager", 1)
+
+  val := new(bool)
+  m.Exec(NewTask(func() error {
+    *val = true
+    return nil
+  }))
+
+  if !*val {
+    t.Error("Did not run the task")
+    return
+  }
+}
