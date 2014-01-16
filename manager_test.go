4390ef2da692678ed29c2cae852d8703fa36322a
Jason Cox <JasonWilburCox@gmail.com>
2014-01-15 19:21:57 -0500

    Testing with more tasks to run

--- manager_test.go
+++ manager_test.go
@@ -11,7 +11,7 @@ func Test_New(t *testing.T) {
   }
 }
 
-func Test_ManagerDoesTask( t *testing.T) {
+func Test_ManagerDoesTask(t *testing.T) {
   m := NewManager("Test Manager", 1)
 
   val := new(bool)
@@ -23,5 +23,32 @@ func Test_ManagerDoesTask( t *testing.T) {
   if !*val {
     t.Error("Did not run the task")
     return
+  }
+}
+
+func Test_ManagerDoesSeveralTasks(t *testing.T) {
+  m := NewManager("Test Manaer", 5)
+
+  vals := make([]*bool, 5)
+  for i := range vals {
+    vals[i] = new(bool)
+  }
+
+  for i := range vals {
+    // No free i's around here....
+    i := i
+    m.Exec(NewTask(func() error {
+      *vals[i] = true
+      return nil
+    }))
+  }
+
+  m.Finish()
+
+  for _, val := range vals {
+    if !*val {
+      t.Error("Did not exec task")
+      return
+    }
   }
 }
