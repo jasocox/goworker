5aecee77aeb1e116edcca5e5cb45ba82bb894b56
Jason Cox <JasonWilburCox@gmail.com>
2014-01-15 19:22:37 -0500

    Make the tasks more useful outside the package

--- task.go
+++ task.go
@@ -8,10 +8,10 @@ type task struct {
   t func() error
 }
 
-func NewTask(t func() error) task {
-  return task{t}
+func NewTask(t func() error) Task {
+  return &task{t}
 }
 
-func (t task) Do() error {
+func (t *task) Do() error {
   return t.t()
 }
