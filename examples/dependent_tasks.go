7cad9e548675f0a3df8442c0752b02dba8a74de2
Jason Cox <JasonWilburCox@gmail.com>
2014-02-13 20:42:15 -0500

    Basic example for dependent tasks

---
+++ examples/dependent_tasks.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/jasocox/goworker"
+	"log"
+	"math/rand"
+	"time"
+)
+
+type task_one int
+
+func (t task_one) Do() error {
+  log.Printf("First task: %d starting...", t)
+
+  time.Sleep(sleep_time())
+
+  log.Printf("First task: %d done.", t)
+
+  worker.Exec(task_two(t))
+
+  return nil
+}
+
+type task_two int
+
+func (t task_two) Do() error {
+  log.Printf("First task: %d starting...", t)
+
+  time.Sleep(sleep_time())
+
+  log.Printf("Second task: %d done.", t)
+
+  return nil
+}
+
+var worker = goworker.NewManager("Dependent Tasks", 10)
+const TASKS = 1000
+
+func sleep_time() time.Duration {
+  return time.Duration(int(time.Millisecond) * (rand.Intn(91) + 10))
+}
+
+func main() {
+  for i := 0; i < TASKS; i++ {
+    worker.Exec(task_one(i))
+  }
+
+  worker.Finish()
+}
